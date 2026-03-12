e16af37c00d322272bdc5e8918235a4f88fa8791
karthikmcakk <karthikmca.kk@gmail.com>
2026-03-12 23:01:49 +0530

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StartupCompanySite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/script.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $("#learnBtn").click(function () {
+
+        alert("Welcome to MyStartup!");
+
+    });
+
+
+    $("#contactForm").submit(function (e) {
+
+        e.preventDefault();
+
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+
+        if (name == "" || email == "" || message == "") {
+            $("#msg").html("Please fill all fields");
+            return;
+        }
+
+        $("#msg").html("Message sent successfully!");
+
+        $("#contactForm")[0].reset();
+
+    });
+
+});
